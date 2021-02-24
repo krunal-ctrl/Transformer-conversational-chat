98f3436e60e6ef1725f9e0aee583fe95a87d444f
krunal-ctrl <krunaljethva90@gmail.com>
2021-02-24 08:43:03 +0530

    deploying on heroku make changes according to heroku added Procfile configure chat.html to take request from heroku app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_socketio import SocketIO, join_room, leave_room
 from googletrans import Translator
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 clients = []
 userdata = {}
 
