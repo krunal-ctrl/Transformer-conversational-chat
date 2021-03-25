b3169f23e862b0d92a3b3522a95957a21e7afca3
krunal-ctrl <50491905+krunal-ctrl@users.noreply.github.com>
2021-03-25 09:01:01 +0530

    Update database.py
    added language option

--- database.py
+++ database.py
@@ -8,11 +8,11 @@ chat_db = client.get_database('ChatApp')
 user_collection = chat_db.get_collection('user')
 
 
-def save_user(username, email, password):
+def save_user(username, email, password, language):
     password_hash = generate_password_hash(password)
-    user_collection.insert_one({'_id': username, 'email': email, 'password': password_hash})
+    user_collection.insert_one({'_id': username, 'email': email, 'password': password_hash, 'lang':language})
 
 
 def get_user(username):
     user_data = user_collection.find_one({'_id': username})
-    return User(user_data['_id'], user_data['email'], user_data['password']) if user_data else None
+    return User(user_data['_id'], user_data['email'], user_data['password'], user_data['lang']) if user_data else None
